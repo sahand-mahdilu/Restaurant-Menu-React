e5da3475b2c6a2f0a5ccef1fc54b5ec9377cf0b9
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-18 11:41:00 +0330

    feat : transition added

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -4,10 +4,10 @@ export default function Categories() {
   return (
     <div className='flex justify-center items-center gap-3'> 
 
-        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
-        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
-        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
-        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+        <button className='text-yellow-700 font-semibold transition-all duration-250 delay-75 hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+        <button className='text-yellow-700 font-semibold transition-all duration-250 delay-75 hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+        <button className='text-yellow-700 font-semibold transition-all duration-250 delay-75 hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+        <button className='text-yellow-700 font-semibold transition-all duration-250 delay-75 hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
 
     </div>
   )
