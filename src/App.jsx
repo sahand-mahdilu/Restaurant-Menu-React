3343d910e97e67d08097db3b1b0cd587a805af2c
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-18 11:37:56 +0330

    feat : categories component ui created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Categories from './Components/Categories'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,10 @@ function App() {
      <h1 className='text-center font-bold text-3xl mt-5'>Our Menu</h1>
      <div className='mt-2 w-20 h-1 bg-yellow-600'></div>
 
+      </div>
+
+      <div className=' categories w-[100%] flex justify-center items-center mt-6'>
+        <Categories/>
       </div>
 
 
--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Categories() {
   return (
-    <div>Categories</div>
+    <div className='flex justify-center items-center gap-3'> 
+
+        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+        <button className='hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>all</button>
+
+    </div>
   )
 }
