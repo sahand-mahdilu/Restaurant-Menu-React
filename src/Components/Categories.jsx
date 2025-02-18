04d0090e61ebf3d36b77ec811b0072ab229a4da6
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-18 12:14:26 +0330

    feat : changing background added

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,23 +1,22 @@
-import React, { act } from 'react'
+import React, { act, use, useState } from "react";
 
-export default function Categories({categories}) {
-
-    
+export default function Categories({ categories }) {
+  const [mainCategorie, setMainCategorie] = useState("all");
 
   return (
-    <div className='flex justify-center items-center gap-3'> 
-
-
-    {categories.map((cat,index)=>{
-        return(
-
-
-            <button key={index}  className='text-yellow-700 font-semibold transition-all duration-250 delay-75 hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white'>{cat}</button>
-
-        )
-    })}
-
-
+    <div className="flex justify-center items-center gap-3">
+      {categories.map((cat, index) => {
+        return (
+          <button
+            key={index}
+            className={`text-yellow-700 font-semibold transition-all duration-250 delay-75 hover:bg-yellow-700 p-2 w-20 rounded-xl hover:text-white ${
+              cat === mainCategorie ? "bg-yellow-700 text-white" : "bg-white"
+            }`}
+          onClick={()=>{setMainCategorie(cat)}} >
+            {cat}
+          </button>
+        );
+      })}
     </div>
-  )
+  );
 }
